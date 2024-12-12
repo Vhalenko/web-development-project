0bb1c71d8a613309958137f68ffad495884db8d1
Vhalenko <galenko.vladislav@gmail.com>
2024-12-12 18:14:50 +0100

    models created

--- app/public/models/TaskModel.php
+++ app/public/models/TaskModel.php
@@ -1,50 +1,108 @@
 <?php
 
 class Task {
-    private $id;
+    private $taskId;
+    private $userId;
     private $title;
     private $description;
+    private $categoryId;
+    private $priority;
+    private $deadline;
+    private $creationDate;
+    private $completionDate;
     private $isCompleted;
+    private $streakContribution;
 
-    public function __construct($id, $title, $description, $isCompleted = false) {
-        $this->id = $id;
+    public function __construct($taskId, $userId, $title, $categoryId, $priority, $creationDate, $isCompleted = false, $description = null, $deadline = null, $completionDate = null, $streakContribution = false) {
+        $this->taskId = $taskId;
+        $this->userId = $userId;
         $this->title = $title;
         $this->description = $description;
+        $this->categoryId = $categoryId;
+        $this->priority = $priority;
+        $this->deadline = $deadline;
+        $this->creationDate = $creationDate;
+        $this->completionDate = $completionDate;
         $this->isCompleted = $isCompleted;
+        $this->streakContribution = $streakContribution;
     }
 
-    // Getters
-    public function getId() {
-        return $this->id;
+    // Getters and Setters
+    public function getTaskId() {
+        return $this->taskId;
+    }
+    public function setTaskId($taskId) {
+        $this->taskId = $taskId;
+    }
+
+    public function getUserId() {
+        return $this->userId;
+    }
+    public function setUserId($userId) {
+        $this->userId = $userId;
     }
 
     public function getTitle() {
         return $this->title;
     }
-
-    public function getDescription() {
-        return $this->description;
-    }
-
-    public function isCompleted() {
-        return $this->isCompleted;
-    }
-
-    // Setters
     public function setTitle($title) {
         $this->title = $title;
     }
 
+    public function getDescription() {
+        return $this->description;
+    }
     public function setDescription($description) {
         $this->description = $description;
     }
 
-    public function setCompleted($isCompleted) {
+    public function getCategoryId() {
+        return $this->categoryId;
+    }
+    public function setCategoryId($categoryId) {
+        $this->categoryId = $categoryId;
+    }
+
+    public function getPriority() {
+        return $this->priority;
+    }
+    public function setPriority($priority) {
+        $this->priority = $priority;
+    }
+
+    public function getDeadline() {
+        return $this->deadline;
+    }
+    public function setDeadline($deadline) {
+        $this->deadline = $deadline;
+    }
+
+    public function getCreationDate() {
+        return $this->creationDate;
+    }
+    public function setCreationDate($creationDate) {
+        $this->creationDate = $creationDate;
+    }
+
+    public function getCompletionDate() {
+        return $this->completionDate;
+    }
+    public function setCompletionDate($completionDate) {
+        $this->completionDate = $completionDate;
+    }
+
+    public function getIsCompleted() {
+        return $this->isCompleted;
+    }
+    public function setIsCompleted($isCompleted) {
         $this->isCompleted = $isCompleted;
     }
 
-    // Example method to save the task to a database
-    public function save() {
-        // Add logic to insert/update the task in the database
+    public function getStreakContribution() {
+        return $this->streakContribution;
+    }
+    public function setStreakContribution($streakContribution) {
+        $this->streakContribution = $streakContribution;
     }
 }
+
--- app/public/models/UserModel.php
+++ app/public/models/UserModel.php
@@ -2,37 +2,108 @@
 
 require_once(__DIR__ . "/BaseModel.php");
 
-class UserModel extends BaseModel
-{
-    public function __construct()
-    {
-        parent::__construct();
+class User {
+    private $userId;
+    private $username;
+    private $email;
+    private $passwordHash;
+    private $profilePicture;
+    private $dateJoined;
+    private $lastLogin;
+    private $achievements; // JSON or list of Achievement objects
+    private $streakCount;
+    private $totalTasksCompleted;
+    private $role;
+
+    public function __construct($userId, $username, $email, $passwordHash, $dateJoined, $role, $profilePicture = null, $achievements = [], $streakCount = 0, $totalTasksCompleted = 0, $lastLogin = null) {
+        $this->userId = $userId;
+        $this->username = $username;
+        $this->email = $email;
+        $this->passwordHash = $passwordHash;
+        $this->profilePicture = $profilePicture;
+        $this->dateJoined = $dateJoined;
+        $this->lastLogin = $lastLogin;
+        $this->achievements = $achievements;
+        $this->streakCount = $streakCount;
+        $this->totalTasksCompleted = $totalTasksCompleted;
+        $this->role = $role;
     }
 
-    public function getAll()
-    {
-        // demo, this would normally come from the database
-        return [
-            [
-                "id" => 1,
-                "email" => "foo@foo.com",
-                "username" => "foo_user"
-            ],
-            [
-                "id" => 2,
-                "email" => "bar@bar.com",
-                "username" => "bar_user"
-            ],
-        ];
+    // Getters and Setters
+    public function getUserId() {
+        return $this->userId;
+    }
+    public function setUserId($userId) {
+        $this->userId = $userId;
     }
 
-    public function get($id)
-    {
-        // demo, this would normally come from the database
-        return             [
-            "id" => 2,
-            "email" => "bar@bar.com",
-            "username" => "bar_user"
-        ];
+    public function getUsername() {
+        return $this->username;
+    }
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getPasswordHash() {
+        return $this->passwordHash;
+    }
+    public function setPasswordHash($passwordHash) {
+        $this->passwordHash = $passwordHash;
+    }
+
+    public function getProfilePicture() {
+        return $this->profilePicture;
+    }
+    public function setProfilePicture($profilePicture) {
+        $this->profilePicture = $profilePicture;
+    }
+
+    public function getDateJoined() {
+        return $this->dateJoined;
+    }
+    public function setDateJoined($dateJoined) {
+        $this->dateJoined = $dateJoined;
+    }
+
+    public function getLastLogin() {
+        return $this->lastLogin;
+    }
+    public function setLastLogin($lastLogin) {
+        $this->lastLogin = $lastLogin;
+    }
+
+    public function getAchievements() {
+        return $this->achievements;
+    }
+    public function setAchievements($achievements) {
+        $this->achievements = $achievements;
+    }
+
+    public function getStreakCount() {
+        return $this->streakCount;
+    }
+    public function setStreakCount($streakCount) {
+        $this->streakCount = $streakCount;
+    }
+
+    public function getTotalTasksCompleted() {
+        return $this->totalTasksCompleted;
+    }
+    public function setTotalTasksCompleted($totalTasksCompleted) {
+        $this->totalTasksCompleted = $totalTasksCompleted;
+    }
+
+    public function getRole() {
+        return $this->role;
+    }
+    public function setRole($role) {
+        $this->role = $role;
     }
 }
